0f8bec0e069b2e2ae3c72b3ade2473005d22251c
AJackTi <dtrong97vn@gmail.com>
2017-08-26 16:36:30 +0700

    Add files via upload

---
+++ learn_english_every_day.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+link = ["hoc.elight.edu.vn", "rong-chang.com/supereasy/", "https://www.englishlistening.com/index.php/listen-to-passages/", "http://www.talkenglish.com/", "https://www.newsinlevels.com/"]
+
+for i in link:
+	webbrowser.open_new_tab(i)
